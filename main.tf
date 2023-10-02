050bf28131b7a3d7b1a322c57143054573fb32f4
Paul Jobb <paul.jobb@gov.ab.ca>
2023-10-02 10:51:47 -0600

    testing stuff

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "null_resource" "export_file" {
   for_each  = { for vm in var.vm : vm.vm_name => vm }
   provisioner "local-exec" {    
     command = <<EOT
-    echo ${yamlencode(each.value.vm_name)} >> /home/paul/work/tf-vmware/terragrunt/vm_to_ceate.yaml
+    echo ${each.value.vm_name}  ${local.generated_password[each.value.vm_name]} ${var.vsphere_cluster}>> /home/paul/work/tf-vmware/terragrunt/vm_to_ceate.txt
 EOT
   }
   
